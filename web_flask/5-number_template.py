6a2af14409065254422171e49baaf782c5f321e6
markbonchi <markbonchi@gmail.com>
2023-02-06 22:57:41 +0000

    Render template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" Flask web application
+"""
+
+
+
+from flask import Flask
+from flask import render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello():
+    """route display
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    """route display
+    """
+    return "HBNB"
+
+@app.route('/c/<text>')
+def print_C(text):
+    """display C followed by <text>
+    """
+    return "C {}".format(escape(text.replace("_", " ")))
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def Python(text):
+    """display  python followed by <text>
+    """
+    return "Python {}".format(escape(text.replace("_", " ")))
+
+@app.route('/number/<int:n>')
+def number(n):
+    """display n if type(int)
+    """
+    return "{} is a number".format(n)
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """display HTML page if n is type(int)
+    """
+    return render_template("5-number.html", n=n)
+
+if __name__ == "__main__":
+	"""run app on port 5000
+	"""
+	app.run(debug=True, port=5000, host='0.0.0.0')
