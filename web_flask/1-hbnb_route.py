c01397310a90104905180ba1f1adae62f6c63185
markbonchi <markbonchi@gmail.com>
2023-02-06 20:29:43 +0000

    New route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Start Flask web application
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+        """route display
+        """
+        return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+	"""route display
+	"""
+	return "HBNB"
+
+if __name__ == "__main__":
+	"""run app on port 5000
+	"""
+	app.run(debug=True, port=5000, host='0.0.0.0')
